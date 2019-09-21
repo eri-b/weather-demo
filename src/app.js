3a93cc63957c6440e3fb96752446f1523aadc162
erib <eri-b@users.noreply.github.com>
2019-09-21 11:51:02 -0400

    gify + styling

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import displayWeather from './display';
+
+const toF = (cel) => {
+  const f = Math.round(cel * (9 / 5) + 32);
+  return f;
+};
+
+const setLocal = (temp) => {
+  localStorage.setItem('ce', Math.round(temp));
+  localStorage.setItem('fa', toF(temp));
+};
+
+const parseWeather = (data) => {
+  const obj = {
+    loc: data.name,
+    temp: data.main.temp,
+    desc: data.weather[0].description,
+    icon: data.weather[0].icon,
+  };
+  setLocal(obj.temp);
+  return obj;
+};
+
+
+const findWeather = (location) => {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=24bc3889266a1fd87fecc9abc9dd9f33&units=metric`, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      const weather = parseWeather(response);
+      displayWeather(weather);
+    })
+    .catch((e) => { console.log('catch: ', e); });
+};
+
+export default findWeather;
--- src/display.js
+++ src/display.js
@@ -1,10 +1,21 @@
+const setGif = (desc) => {
+  const img = document.querySelector('#gif');
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=7aFa3m21bGaw1XdvW85BAmOs6fQl3UtR&s=${desc}`, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => {
+      img.src = response.data.images.original.url;
+    })
+    .catch((e) => { console.log('catch: ', e); });
+};
+
 const displayWeather = (weather) => {
-  console.log(weather.loc);
   document.querySelector('#loc').innerHTML = weather.loc;
   document.querySelector('#temp').innerHTML = `${Math.round(weather.temp)}&#176;C`;
-  document.querySelector('#desc').innerHTML = weather.desc;
+  document.querySelector('#desc').innerHTML = weather.desc.toUpperCase();
   const iconurl = `http://openweathermap.org/img/w/${weather.icon}.png`;
   document.querySelector('#icon img').src = iconurl;
+  setGif(weather.desc);
 };
+
 
 export default displayWeather;
